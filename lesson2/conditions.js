896d413e3a8d527e22977c32876016b307346599
AnatoliiAnitsai <anatolii.anitsai@gmail.com>
2021-01-28 02:50:06 +0200

    add fix conditions

--- lesson2/conditions.js
+++ lesson2/conditions.js
@@ -2,20 +2,32 @@ let inputStr = "123";
 
 if (inputStr.trim().slice(0, 2).toUpperCase() == "UP") {
     console.log("up");
-}else if (inputStr.trim().slice(0, 4).toUpperCase() == "DOWN"){
+} else if (inputStr.trim().slice(0, 4).toUpperCase() == "DOWN") {
     console.log("down \nstop");
-}else{
+} else {
     console.log("stop");
 }
 
-let inputValue = 12;
+let inputValue = 18;
 
-if((typeof inputValue) == "string"){
+if ((typeof inputValue) == "string") {
     console.log("string: " + inputValue.toLowerCase());
-}else {
+} else {
     if (typeof inputValue == "number")
-       switch (inputValue % 10){
-           case (2 || 4):
-               console.log("testhhhhhhhhhhhh");
-       }
+        switch (inputValue % 10) {
+            case 2:
+            case 4:
+                console.log("less than five");
+                break;
+            case 6:
+            case 8:
+                console.log("great than five");
+                break;
+            case(0):
+                console.log("multiple of ten");
+                break;
+            default:
+                console.log("odd number");
+                break;
+        }
 }
